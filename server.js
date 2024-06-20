951a43ad7cd273c5e313197673389c420a94af24
Milton Yama <miltondyama@gmail.com>
2024-06-20 16:07:08 -0400

    uploading projecto to github

---
+++ server.js
@@ -0,0 +1,71 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({
+    message:
+      'API WORKING!! case u found any problem, pls send me on https://github.com/Myllth0m/api-pra-x/issues',
+  })
+})
+
+app.post('/users', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  })
+
+  res.status(201).json(req.body)
+})
+
+app.get('/users', async (req, res) => {
+  let users = []
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        email: req.query.email,
+        name: req.query.name,
+        age: req.query.age,
+      },
+    })
+  } else {
+    users = await prisma.user.findMany()
+  }
+
+  res.status(200).json(users)
+})
+
+app.put('/users/:id', async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  })
+
+  res.status(200).json(req.body)
+})
+
+app.delete('/users/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  })
+
+  res.status(200).json({ message: 'User deleted successfully!' })
+})
+
+app.listen(3000)
